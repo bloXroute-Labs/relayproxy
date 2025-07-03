900c41b8b332bdc5581aff272249baf6c5806a93
colin <colin.kelly@bloxroute.com>
2025-07-03 12:05:42 -0400

    more

--- service.go
+++ service.go
@@ -2898,6 +2898,10 @@ func (s *Service) prefetchPayload(
 		for i := 0; i < 5 && !exitSignal; i++ {
 			childCtx, childSpan := s.tracer.Start(spanctx, "PreFetchGetPayload")
 			_, reqSpan := s.tracer.Start(childCtx, "PreFetchGetPayload-request")
+			childSpan.SetAttributes(
+				attribute.String("url", client.URL),
+				attribute.String("nodeID", client.NodeID),
+			)
 			out, err := client.PreFetchGetPayload(clientCtx, req)
 			reqSpan.End()
 			if exitSignal {
