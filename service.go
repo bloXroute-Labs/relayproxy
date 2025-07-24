0cab80d1f84c94961b707e10250ebba42bc10f9c
Bhakiyaraj Kalimuthu <bhakiyaraj.kalimuthu@bloxroute.com>
2025-07-24 23:22:42 +0200

    modify proposerRequestStartTimeUnixMS to int64

--- service.go
+++ service.go
@@ -122,7 +122,7 @@ type Service struct {
 	blockPublishingGatewayClient interface{}
 	gatewayAuthKey               string
 	blockPublishFunc             func(tracer trace.Tracer, logger zerolog.Logger, payloadInfo *common.VersionedPayloadInfo, signedBeaconBlock *common.VersionedSignedBlindedBeaconBlock, blockPublishingGatewayClient interface{}, authKey string)
-	OnPayloadDelivered           func(slot uint64, blockHash string, parentHash string, proposerPubkey string, getPayloadRequestClientIP string, receivedAt time.Time, payload []byte, ProposerRequestStartTimeUnixMS, validatorID string) error
+	OnPayloadDelivered           func(slot uint64, blockHash string, parentHash string, proposerPubkey string, getPayloadRequestClientIP string, receivedAt time.Time, payload []byte, ProposerRequestStartTimeUnixMS int64, validatorID string) error
 }
 
 type slotStatsEvent struct {
@@ -1464,7 +1464,8 @@ func (s *Service) GetPayload(ctx context.Context, log *zerolog.Logger, in *Paylo
 			pub := miniSlotDuty.Registration.Message.Pubkey
 			pubkeyStr = pub.String()
 		}
-		err = s.OnPayloadDelivered(uint64(slot), blockHashStr, parentHashStr, pubkeyStr, in.ClientIP, in.ReceivedAt, in.Payload, in.GetPayloadStartTimeUnixMS, in.ValidatorID)
+		proposerRequestStartTimeUnixMS, _ := strconv.ParseInt(in.GetPayloadStartTimeUnixMS, 10, 64)
+		err = s.OnPayloadDelivered(uint64(slot), blockHashStr, parentHashStr, pubkeyStr, in.ClientIP, in.ReceivedAt, in.Payload, proposerRequestStartTimeUnixMS, in.ValidatorID)
 		if err != nil {
 			log.Error().Err(err).Msg("failed to call OnPayloadDelivered")
 		}
