d8a41d2ba93addbd0811a42e86755a9ba3d5ddd9
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2025-12-04 17:47:21 +0100

    Fulu updates (#88)
    * fall back to fulu
    
    * remove deneb
    
    * remove commented code
    
    ---------
    
    Co-authored-by: jzblox <106985178+jzblox@users.noreply.github.com>

--- common/block_submitter.go
+++ common/block_submitter.go
@@ -68,18 +68,12 @@ func (b *BlockSubmission) UnmarshalSSZ(input []byte, out *VersionedSubmitBlockRe
 		}
 	}
 
-	denebRequest := new(apideneb.SubmitBlockRequest)
-	if err = denebRequest.UnmarshalSSZ(input); err == nil {
-		out.Version = spec.DataVersionDeneb
-		out.Deneb = denebRequest
-		return nil
-	}
-
-	// Fallback Electra fast path (if first attempt failed but payload was Electra)
-	electraRequest := new(builderApiElectra.SubmitBlockRequest)
-	if err = b.unmarshalSSZFast(electraRequest, input); err == nil {
-		out.Version = spec.DataVersionElectra
-		out.Electra = electraRequest
+	// Fallback fulu fast path (if first attempt failed but payload was Fulu)
+	fuluRequest := new(builderApiFulu.SubmitBlockRequest)
+	if err = b.unmarshalSSZFastFulu(fuluRequest, input); err == nil {
+		// Adjust this if you have a dedicated Fulu data version.
+		out.Version = spec.DataVersionFulu
+		out.Fulu = fuluRequest
 		return nil
 	}
 
