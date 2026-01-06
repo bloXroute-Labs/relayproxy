a30d4aadd56d2c1c5824ccd3e734171d5fc2319f
jzblox <106985178+jzblox@users.noreply.github.com>
2026-01-06 11:54:32 -0500

    MEV-1687 add adjustment

--- common/versioned_signed_builder_bid.go
+++ common/versioned_signed_builder_bid.go
@@ -241,3 +241,34 @@ func (r *VersionedSignedBuilderBid) ExcessBlobGas() (uint64, error) {
 		return 0, errors.Wrap(ErrInvalidVersion, fmt.Sprintf("%s is not supported", r.Version))
 	}
 }
+
+func (r *VersionedSignedBuilderBid) ExecutionPayloadHeader() (*deneb.ExecutionPayloadHeader, error) {
+	switch r.Version {
+	case spec.DataVersionFulu:
+		if r.Fulu == nil {
+			return nil, errors.New("no data")
+		}
+		if r.Fulu.Message == nil {
+			return nil, errors.New("no data message")
+		}
+		return r.Fulu.Message.Header, nil
+	case spec.DataVersionElectra:
+		if r.Electra == nil {
+			return nil, errors.New("no data")
+		}
+		if r.Electra.Message == nil {
+			return nil, errors.New("no data message")
+		}
+		return r.Electra.Message.Header, nil
+	case spec.DataVersionDeneb:
+		if r.Deneb == nil {
+			return nil, errors.New("no data")
+		}
+		if r.Deneb.Message == nil {
+			return nil, errors.New("no data message")
+		}
+		return r.Deneb.Message.Header, nil
+	default:
+		return nil, errors.Wrap(ErrInvalidVersion, fmt.Sprintf("%s is not supported", r.Version))
+	}
+}
